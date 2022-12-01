e2e6a72937d68b242fa52535432b0df150816627
Abdulkhay Payziev <84310155+apayziev@users.noreply.github.com>
2022-12-01 14:23:17 +0500

    solution for the first problem set

--- day1/solution.py
+++ day1/solution.py
@@ -1,10 +1,9 @@
-# with open("./input.txt", "r") as file:
-# problem set 1
-
-# num_list = {
-#     f"Elf-{str(index)}": sum(map(int, num_list.split("\n")))
-#     for index, num_list in enumerate(file.read().strip().split("\n\n"), start=1)
-# }
+with open("./input.txt", "r") as file:
+  # problem set 1
+  # num_list = {
+  #     f"Elf-{str(index)}": sum(map(int, num_list.split("\n")))
+  #     for index, num_list in enumerate(file.read().strip().split("\n\n"), start=1)
+  # }
 
 # print(num_list[max(num_list, key=num_list.get)])
 
